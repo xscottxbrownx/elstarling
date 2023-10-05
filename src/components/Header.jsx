e5999bb8938d61211467ef6c4bc5fc5f5ddad745
veganedge <scottxbrown@yahoo.com>
2023-10-05 16:58:30 -0700

    Adding links to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,42 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Spacer, HStack } from "@chakra-ui/react";
 import signature from "../assets/signatureWhite.webp";
 import { Image } from "@chakra-ui/react";
+import LINKS from "../constants/pageLinks";
 
 const Header = () => (
   <Box
     as="header"
     pos="fixed"
-    zIndex="201"
+    zIndex="200"
     bg="transparent"
-    // bg="lightseagreen"
+    backdropFilter="blur(25px) invert(.15)"
+    w="100vw"
+    h={{ base: "3rem", lg: "5rem" }}
+    color="white"
   >
-    <Image
-      src={signature}
-      alt="E L Starling signature"
-      h={{ base: "40px", lg: "70px" }}
-      fallbackSrc="https://via.placeholder.com/70"
-      ml="2rem"
-      mt={{ base: "3px", lg: "null" }}
-    />
+    <Flex>
+      <Image
+        src={signature}
+        alt="E L Starling signature"
+        h={{ base: "40px", lg: "70px" }}
+        fallbackSrc="https://via.placeholder.com/70"
+        ml={{ base: "1rem", lg: "2rem" }}
+        mt={{ base: "3px", lg: "null" }}
+      />
+      <Spacer />
+      <HStack spacing={{ base: 3, lg: 6 }} mr={{ base: "1rem", lg: "2rem" }}>
+        {LINKS.map((link) => (
+          <Box
+            as="a"
+            key={link}
+            href={`#${link}`}
+            _hover={{ textDecoration: "underline" }}
+          >
+            {link}
+          </Box>
+        ))}
+      </HStack>
+    </Flex>
   </Box>
 );
 
