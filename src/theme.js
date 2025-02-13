b1690ea4535ba9934b7ee42c8d9e3e8e6484371f
veganedge <scottxbrown@yahoo.com>
2025-02-13 13:17:28 -0800

    Adding in 2 fonts, and theming the body and headers to these fonts

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "'Lora', serif",
+    body: "'Merriweather', serif",
+  },
+});
+
+export default theme;
