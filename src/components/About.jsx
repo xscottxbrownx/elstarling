b657d5835713a5e7483a7fc53026a37944a85203
veganedge <scottxbrown@yahoo.com>
2025-02-15 12:08:36 -0800

    adjust text length based on mobile or desktop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,48 +1,65 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text, useBreakpointValue } from "@chakra-ui/react";
 import AboutStar from "../assets/AboutStar.webp";
 
-// ****** conditional render link to bio (or shorter bio) if mobile view ******
+const About = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
-const About = () => (
-  <Flex
-    as="section"
-    id="about"
-    direction={{ base: "column", lg: "row" }}
-    w="100vw"
-    align="center"
-    justify="center"
-    mb="2.5rem"
-    px={{ base: "2rem", lg: "5rem", xl: "18rem" }}
-    py="6rem"
-    bg="var(--background)"
-  >
-    <Image
-      src={AboutStar}
-      alt="About the Author"
-      mt="2rem"
-      w={{ base: "75%", md: "45%", lg: "30%" }}
-      height="auto"
-    />
-    <Box
-      maxW={{ base: "400px", md: "600px", xl: "1000px" }}
-      mt={{ base: "2rem", lg: "0" }}
-      px={{ base: "1rem", lg: "4rem" }}
+  const renderMobileText = (
+    <Text color="var(--primary)" sx={{ lineHeight: "2", fontSize: "18px" }}>
+      <Text as="b" sx={{ fontWeight: "extrabold" }}>
+        Emily “E.L.” Starling
+      </Text>{" "}
+      lives in Portland, Oregon with her high school sweetheart and an orange
+      cat named Pigeon. In her pre-dawn hours, writes speculative fiction full
+      of longing and twisty secrets with a cup of coffee at the ready and the
+      ever-present hope of pastries.{" "}
+    </Text>
+  );
+
+  const renderDesktopText = (
+    <Text color="var(--primary)" sx={{ lineHeight: "2", fontSize: "18px" }}>
+      <Text as="b" sx={{ fontWeight: "extrabold" }}>
+        Emily “E.L.” Starling
+      </Text>{" "}
+      grew up in the center of California, midway between all the cities anyone
+      has ever heard of. Today she lives in Portland, Oregon with her high
+      school sweetheart and a needy orange cat named Pigeon. She spends her days
+      papercutting, screen-printing, running slowly, and hiking just as slowly.
+      In her pre-dawn hours, she can be found writing YA speculative fiction
+      novels full of longing and twisty secrets with a cup of coffee at the
+      ready and the ever-present hope of pastries.{" "}
+    </Text>
+  );
+
+  return (
+    <Flex
+      as="section"
+      id="about"
+      direction={{ base: "column", lg: "row" }}
+      w="100vw"
+      align="center"
+      justify="center"
+      mb="2.5rem"
+      px={{ base: "2rem", lg: "5rem", xl: "18rem" }}
+      py="6rem"
+      bg="var(--background)"
     >
-      <Text color="var(--primary)" sx={{ lineHeight: "2", fontSize: "18px" }}>
-        <Text as="b" sx={{ fontWeight: "extrabold" }}>
-          Emily “E.L.” Starling
-        </Text>{" "}
-        grew up in the center of California, midway between all the cities
-        anyone has ever heard of. Today she lives in Portland, Oregon with her
-        family and, often, a borrowed cat named Spock who enjoys sneaking into
-        her home to avoid the rain. She spends her days papercutting,
-        screen-printing, running slowly, and hiking just as slowly. In her
-        pre-dawn hours, she can be found writing YA speculative fiction novels
-        full of longing and twisty secrets with a cup of coffee at the ready and
-        the ever-present hope of pastries.{" "}
-      </Text>
-    </Box>
-  </Flex>
-);
+      <Image
+        src={AboutStar}
+        alt="About the Author"
+        mt="2rem"
+        w={{ base: "75%", md: "45%", lg: "30%" }}
+        height="auto"
+      />
+      <Box
+        maxW={{ base: "400px", md: "600px", xl: "1000px" }}
+        mt={{ base: "2rem", lg: "0" }}
+        px={{ base: "1rem", lg: "4rem" }}
+      >
+        {isMobile ? renderMobileText : renderDesktopText}
+      </Box>
+    </Flex>
+  );
+};
 
 export default About;
