8a8ae56bff7f27214a309a8109ff8c42b2da3f05
veganedge <scottxbrown@yahoo.com>
2025-02-15 10:18:59 -0800

    adjusting button sizes/colors

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,11 +4,15 @@ import {
   Image,
   Heading,
   HStack,
+  Text,
   useBreakpointValue,
   VStack,
+  Button,
 } from "@chakra-ui/react";
 import BooksStar from "../assets/BooksStar.webp";
 import BookCover from "../assets/BoundByStars.webp";
+import { FaPinterest, FaSpotify } from "react-icons/fa";
+import { GiBrain } from "react-icons/gi";
 
 const Books = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
@@ -26,13 +30,30 @@ const Books = () => {
 
   const renderDesktop = (
     <HStack>
-      <Box flex="2">
+      <Box flex="3">
         <Image src={BookCover} alt="Bound By Stars book" objectFit="cover" />
       </Box>
-      <VStack bg="purple" flex="2">
-        <Heading color="var(--secondary)">Genius Links Here</Heading>
-        <Heading color="var(--secondary)">Spotify Link Here</Heading>
-        <Heading color="var(--secondary)">Pinterest Link Here</Heading>
+      <VStack flex="1" py="1rem" alignItems="start">
+        <Button border="solid black 1px" h="56px" w="280px">
+          <GiBrain size="36px" color="var(--secondary)" />
+          <Text color="var(--secondary)" ml="10px" fontSize="24px">
+            Preorder
+          </Text>
+        </Button>
+        <Button border="solid black 1px" h="56px" w="280px">
+          {/* <FaSpotify size="48px" color="#1ED760" /> */}
+          <FaSpotify size="36px" color="var(--secondary)" />
+          <Text color="var(--secondary)" ml="10px" fontSize="24px">
+            Playlist
+          </Text>
+        </Button>
+        <Button border="solid black 1px" h="56px" w="280px">
+          {/* <FaPinterest size="48px" color="#E60023" /> */}
+          <FaPinterest size="36px" color="var(--secondary)" />
+          <Text color="var(--secondary)" ml="10px" fontSize="24px">
+            Moodboard
+          </Text>
+        </Button>
       </VStack>
     </HStack>
   );
@@ -45,7 +66,7 @@ const Books = () => {
       my="2.5rem"
       pt="2rem"
       pb={isMobile ? "2rem" : "1rem"}
-      px={{ base: "2rem", lg: "5rem", xl: "8rem" }}
+      px={{ base: "2rem", lg: "5rem", xl: "18rem" }}
       align="center"
       direction="column"
       bg="var(--background)"
