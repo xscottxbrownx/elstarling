b497d8c9610cb73f63fe8c887234a15e8402e8c7
veganedge <scottxbrown@yahoo.com>
2025-02-15 07:36:06 -0800

    Substack signup embed added

--- src/components/Contact2.jsx
+++ src/components/Contact2.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Flex,
   HStack,
   IconButton,
@@ -11,8 +12,6 @@ import {
 } from "@chakra-ui/react";
 import contactLinks from "../constants/contactLinks";
 import ContactStar from "../assets/ContactStar.webp";
-
-// ***** add signup for substack? *****
 
 const Contact2 = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
@@ -28,33 +27,12 @@ const Contact2 = () => {
       justify="space-evenly"
       gap={24}
     >
-      <VStack spacing={4} ml={{ base: "0", lg: "2rem" }}>
+      <VStack spacing={4}>
         <Image
           src={ContactStar}
           alt="Contact the author"
           w={{ base: "75%", md: "50%" }}
         />
-        <HStack align="center" justify="space-around" direction="row">
-          {contactLinks.map(({ label, href, icon }) => (
-            <Tooltip key={label} label={label} closeOnClick={false} hasArrow>
-              <IconButton
-                as="a"
-                href={href}
-                target="_blank"
-                rel="noreferrer"
-                aria-label={label.toLowerCase()}
-                variant="ghost"
-                size="lg"
-                fontSize="3xl"
-                icon={icon}
-                color="var(--tertiary)"
-                bg="var(--blue)"
-                _hover={{ bg: "var(--tertiary)", color: "white" }}
-                isRound
-              />
-            </Tooltip>
-          ))}
-        </HStack>
         {isMobile ? (
           <>
             <VStack>
@@ -83,6 +61,34 @@ const Contact2 = () => {
             </HStack>
           </>
         )}
+        <HStack align="center" justify="space-around" direction="row">
+          {contactLinks.map(({ label, href, icon }) => (
+            <Tooltip key={label} label={label} closeOnClick={false} hasArrow>
+              <IconButton
+                as="a"
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+                aria-label={label.toLowerCase()}
+                variant="ghost"
+                size="lg"
+                fontSize="3xl"
+                icon={icon}
+                color="var(--tertiary)"
+                bg="var(--blue)"
+                _hover={{ bg: "var(--tertiary)", color: "white" }}
+                isRound
+              />
+            </Tooltip>
+          ))}
+        </HStack>
+        <Box width={{ base: "300px", md: "450px" }} mt="2rem">
+          <iframe
+            src="https://elstarling.substack.com/embed"
+            width="100%"
+            height="150"
+          />
+        </Box>
       </VStack>
     </Flex>
   );
