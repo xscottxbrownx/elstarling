f392e74e76ada95fee22aac997dad7aee44b6d9f
veganedge <scottxbrown@yahoo.com>
2025-02-16 12:06:29 -0800

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import About from "./components/About";
-// import Contact from "./components/Contact";
 import Contact2 from "./components/Contact2";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-// import Posts from "./components/Posts";
 import Posts2 from "./components/Posts2";
 import Socials from "./components/Socials";
 import Books from "./components/Books";
@@ -20,9 +18,7 @@ const App = () => (
       <About />
       <ImageBreak />
       <Books />
-      {/* <Posts /> */}
       <Posts2 />
-      {/* <Contact /> */}
       <Contact2 />
     </main>
     <Footer />
