73172f868044c5ff67d64a0fe16713f6da100f38
veganedge <scottxbrown@yahoo.com>
2025-02-17 20:30:11 -0800

    adjusted footer logo to be center of website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,15 @@ import {
   Stack,
   Text,
   useBreakpointValue,
+  VStack,
 } from "@chakra-ui/react";
 import logoSquare from "../assets/ELSsmallSquareLogoBLUE.webp";
 
-export default function Footer() {
+export default function Footer2() {
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   const renderMobile = (
-    <>
+    <VStack spacing={4}>
       <Image
         src={logoSquare}
         alt="E L Starling square logo"
@@ -22,32 +23,32 @@ export default function Footer() {
         fallbackSrc="https://placehold.co/75"
       />
       <Text>© 2025 E L Starling, All rights reserved.</Text>
-    </>
+      <HStack>
+        <Text>Built by:</Text>
+        <Link
+          href="https://www.scottxbrown.com"
+          isExternal
+          aria-label="SxB Development"
+          _hover={{
+            textDecoration: "underline",
+          }}
+        >
+          SxB Development
+        </Link>
+      </HStack>
+    </VStack>
   );
 
   const renderDesktop = (
     <>
-      <Text>© 2025 E L Starling, All rights reserved.</Text>
-      <Image
-        src={logoSquare}
-        alt="E L Starling logo"
-        h="75px"
-        fallbackSrc="https://placehold.co/75"
-      />
-    </>
-  );
-
-  return (
-    <Box as="footer" color="var(--secondary)">
-      <Container
-        as={Stack}
-        maxW={"6xl"}
-        py={4}
-        direction={{ base: "column", md: "row" }}
-        justify={{ base: "center", md: "space-between" }}
-        align="center"
+      <HStack
+        justify="space-between"
+        w="full"
+        maxW="6xl"
+        my="1rem"
+        alignItems="flex-end"
       >
-        {isMobile ? renderMobile : renderDesktop}
+        <Text>© 2025 E L Starling, All rights reserved.</Text>
         <HStack>
           <Text>Built by:</Text>
           <Link
@@ -61,6 +62,29 @@ export default function Footer() {
             SxB Development
           </Link>
         </HStack>
+      </HStack>
+      <Box position="absolute" left="50%" transform="translateX(-50%)">
+        <Image
+          src={logoSquare}
+          alt="E L Starling logo"
+          h="75px"
+          fallbackSrc="https://placehold.co/75"
+        />
+      </Box>
+    </>
+  );
+
+  return (
+    <Box as="footer" color="var(--secondary)" position="relative">
+      <Container
+        as={Stack}
+        maxW="6xl"
+        py={4}
+        direction={{ base: "column", md: "row" }}
+        justify={{ base: "center", md: "space-between" }}
+        align="center"
+      >
+        {isMobile ? renderMobile : renderDesktop}
       </Container>
     </Box>
   );
